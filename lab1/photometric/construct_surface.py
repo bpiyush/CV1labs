7f210b0a0178cf5d1aa6650daae6cdb2934e2a0c
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-09-14 11:27:29 +0200

    WIP: Part 1.3 first pass done. Not getting expected results

--- lab1/photometric/construct_surface.py
+++ lab1/photometric/construct_surface.py
@@ -28,18 +28,67 @@ def construct_surface(p, q, path_type='column'):
         %       height_value = previous_height_value + corresponding_p_value
         
         """
+        height_map[0, 0] = 0.0
+        for y in range(1, h):
+            height_map[y, 0] = height_map[y - 1, 0] + q[y, 0]
+        
+        for y in range(h):
+            for x in range(1, w):
+                height_map[y, x] = height_map[y, x - 1] + p[y, x]
+
     elif path_type=='row':
         """
         ================
         Your code here
         ================
         """
+        height_map[0, 0] = 0.0
+        for x in range(1, w):
+            height_map[0, x] = height_map[0, x - 1] + p[0, x]
+        
+        for x in range(w):
+            for y in range(1, h):
+                height_map[y, x] = height_map[y - 1, x] + q[y, x]
+
     elif path_type=='average':
         """
         ================
         Your code here
         ================
         """
+        height_map_col = np.zeros([h, w])
+        height_map_row = np.zeros([h, w])
+
+        # compute col-wise
+        height_map_col[0, 0] = 0.0
+        for y in range(1, h):
+            height_map_col[y, 0] = height_map_col[y - 1, 0] + q[y, 0]
+        for y in range(h):
+            for x in range(1, w):
+                height_map_col[y, x] = height_map_col[y, x - 1] + p[y, x]
+
+        # compute row-wise
+        height_map_row[0, 0] = 0.0
+        for y in range(1, h):
+            height_map_row[y, 0] = height_map_row[y - 1, 0] + p[y, 0]
+        for y in range(h):
+            for x in range(1, w):
+                height_map_row[y, x] = height_map_row[y, x - 1] + q[y, x]
+
+        height_map = (height_map_col + height_map_row) / 2.0
         
     return height_map
         
+
+if __name__ == "__main__":
+    p = np.random.randn(10, 10)
+    q = np.random.randn(10, 10)
+
+    height_map = construct_surface(p, q, "column")
+    assert height_map.shape == (10, 10)
+
+    height_map = construct_surface(p, q, "row")
+    assert height_map.shape == (10, 10)
+
+    height_map = construct_surface(p, q, "average")
+    assert height_map.shape == (10, 10)
