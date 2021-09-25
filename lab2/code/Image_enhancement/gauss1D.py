bf8d834c458750ef4c207c6144f3ab9216b15e9e
Mark <mark@syftanalytics.com>
2021-09-25 22:38:28 +0200

    update of gauss2d

--- lab2/code/Image_enhancement/gauss1D.py
+++ lab2/code/Image_enhancement/gauss1D.py
@@ -1,9 +1,13 @@
 import numpy as np
 
-def gauss1D( sigma , kernel_size )
+
+def gauss1D(sigma, kernel_size):
     G = np.zeros((1, kernel_size))
-    if kernel_size % 2 == 0
+    if (kernel_size % 2 == 0):
         raise ValueError('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     # solution
-	
-	return G
+    x = np.linspace(1, kernel_size, kernel_size)
+    x = x - round(kernel_size / 2) - 1
+    G = np.exp((-(x ** 2)) / ((2 * (sigma ** 2)))) / (np.sqrt(2 * np.pi) * sigma)
+    G = G / np.sum(G)
+    return G
--- lab2/code/Image_enhancement/gauss2D.py
+++ lab2/code/Image_enhancement/gauss2D.py
@@ -1,3 +1,6 @@
-def gauss2D( sigma_x, sigma_y , kernel_size ):
-    ## solution
-    return G
+import numpy as np
+from gauss1D import gauss1D
+
+
+def gauss2D(sigma_x, sigma_y, kernel_size):
+    return np.outer(gauss1D(sigma_x, kernel_size), gauss1D(sigma_y, kernel_size))
