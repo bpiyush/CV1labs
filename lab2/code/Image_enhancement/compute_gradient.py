5be6a4e337343fe27ef633b42f6e748cd6b17d25
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-09-25 17:51:28 +0200

    Completed expt for Question 8

--- lab2/code/Image_enhancement/compute_gradient.py
+++ lab2/code/Image_enhancement/compute_gradient.py
@@ -2,20 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal
 import cv2
+import matplotlib.pyplot as plt
 
 
 def compute_gradient(image):
     Sx = [[1, 0, -1], [2, 0, -2], [1, 0, -1]]
     Sy = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
 
+    if image.dtype in [int, "uint8"]:
+        image = image.astype("float32") / 255.0
+
     Gx = scipy.signal.convolve2d(image, Sx, mode='same')
     Gy = scipy.signal.convolve2d(image, Sy, mode='same')
+
     im_magnitude = np.sqrt(np.square(Gx) + np.square(Gy))
-    im_direction = np.arctan(Gy / Gx)
+    assert np.isnan(im_magnitude).sum() == 0
+
+    im_direction = np.arctan(Gy / (Gx + np.finfo(float).eps))
+
     return Gx, Gy, im_magnitude, im_direction
 
 
 if __name__ == '__main__':
-    salt_and_pepper = cv2.imread('images/image1_saltpepper.jpg', cv2.IMREAD_GRAYSCALE)
-    salt_and_pepper = np.array(salt_and_pepper, dtype='float32')
-    compute_gradient(salt_and_pepper)
+
+    image = cv2.imread("images/image2.jpg", cv2.IMREAD_GRAYSCALE)
+    Gx, Gy, im_magnitude, im_direction = compute_gradient(image)
+    
+    images = [image, Gx, Gy, im_magnitude, im_direction]
+    labels = ["Original image", "$G_x$", "$G_y$", "Magnitude", "Direction"]
+
+    fig, ax = plt.subplots(1, 5, figsize=(20, 5), constrained_layout=True)
+    for i, _ax in enumerate(ax):
+        _ax.axis("off")
+        _ax.imshow(images[i])
+        _ax.set_title(labels[i], fontsize=18)
+
+    plt.savefig("results/gradient.png", bbox_inches="tight")
+    plt.show()
