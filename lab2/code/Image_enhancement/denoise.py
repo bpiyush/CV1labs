4eb1acdd047b8d661bfc30f98dd9fd8c52eef557
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-09-25 14:58:33 +0200

    Minor corrections and plotting for question 4.2.2

--- lab2/code/Image_enhancement/denoise.py
+++ lab2/code/Image_enhancement/denoise.py
@@ -5,23 +5,44 @@ import matplotlib.pyplot as plt
 
 def denoise(image, kernel_type, k):
     if kernel_type == 'box':
-        out = cv2.blur(image, (k, k))
+        out = cv2.blur(src=image, ksize=(k, k))
     elif kernel_type == 'median':
-        out = cv2.medianBlur(image, (k, k))
+        out = cv2.medianBlur(src=image, ksize=k)
     elif kernel_type == 'gaussian':
-        out = cv2.GaussianBlur(image, (k, k))
+        out = cv2.GaussianBlur(src=image, ksize=(k, k))
     else:
         print('Operation not implemented')
     return out
 
 
 if __name__ == '__main__':
-    salt_and_pepper = cv2.imread('images/image1_saltpepper.jpg', cv2.IMREAD_GRAYSCALE)
-    image_one = cv2.imread('images/image1.jpg', cv2.IMREAD_GRAYSCALE)
-    gaussian = cv2.imread('images/image1_gaussian.jpg', cv2.IMREAD_GRAYSCALE)
-    salt_and_pepper = np.array(salt_and_pepper, dtype='float32')
-    image_one = np.array(image_one, dtype='float32')
-    gaussian = np.array(gaussian, dtype='float32')
-    for k in [3, 5, 7]:
-        plt.imshow(denoise(image_one, 'box', k))
-        plt.show()
+
+    noise_names = {
+        "saltpepper": "Salt And Pepper",
+        "gaussian": "Gaussian",
+    }
+
+    kernel_type = "box"
+    convert_to_float = False
+
+    noise_type = "saltpepper"
+    noisy_image = cv2.imread(f'images/image1_{noise_type}.jpg', cv2.IMREAD_GRAYSCALE)
+
+    ksizes = [3, 5, 7]
+    denoised_outputs = []
+    for k in ksizes:
+        denoised_outputs.append(denoise(noisy_image, kernel_type, k))
+
+    fig, ax = plt.subplots(1, 4, figsize=(20, 4), constrained_layout=True)
+
+    ax[0].axis("off")
+    ax[0].set_title(f"Noisy image ({noise_names[noise_type]})", fontsize=17)
+    ax[0].imshow(noisy_image)
+
+    for i, _ax in enumerate(ax[1:]):
+        _ax.axis("off")
+        _ax.imshow(denoised_outputs[i])
+        _ax.set_title(f"${ksizes[i]}\\times{ksizes[i]}$", fontsize=17)
+    
+    plt.savefig(f"results/image1_{noise_type}_{kernel_type}.png", bbox_inches="tight")
+    plt.show()
