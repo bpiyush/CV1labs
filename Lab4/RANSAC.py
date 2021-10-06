2786035452019c8d7b22e368d21b52881ed794fb
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-10-06 23:50:55 +0200

    Still struggling with RANSAC

--- Lab4/RANSAC.py
+++ Lab4/RANSAC.py
@@ -7,25 +7,6 @@ from keypoint_matching import KeypointMatcher
 from utils import show_single_image, show_two_images
 
 
-# def homogenize(X: np.ndarray):
-#     assert len(X.shape) == 2 and X.shape[-1] == 2
-#     X_homo = np.hstack([X, np.ones((len(X), 1))])
-#     return X_homo
-
-
-# def inv_homogenize(X: np.ndarray):
-#     assert len(X.shape) == 2 and X.shape[-1] == 3
-#     X = X[:, :-1] / X[:, -1]
-#     return X
-
-
-# def rigid_body_transform(X, RT):
-#     """Performs rigid body transformation of points X (N x 3) using RT (3x3 matrix)"""
-#     X = homogenize(X)
-#     X_ = np.matmul(X, RT.T)
-#     X_ = inv_homogenize(X_)
-#     return X_
-
 def project_2d_to_6d(X: np.ndarray):
     """Projects X (N x 2) to Z (2N x 6) space."""
     N = len(X)
@@ -107,18 +88,6 @@ def rigid_body_transform_params(X1: np.ndarray, X2: np.ndarray):
     return params
 
 
-# def get_rigid_transform_params(X1, X2):
-#     # X1: {(x, y)}: N x 2
-#     # X2: {(x', y')}: N x 2
-#     # X2 = (RT) X1
-#     X1_homo = np.hstack([X1, np.ones((len(X1), 1))])
-#     X2_homo = np.hstack([X2, np.ones((len(X2), 1))])
-
-#     RT = np.dot(np.linalg.pinv(X1_homo), X2_homo).T
-#     return RT
-
-
-
 class ImageAlignment:
     """Class to perform alignment of a pair of images given keypoints."""
 
@@ -152,7 +121,7 @@ class ImageAlignment:
 
         return RT, X1_homo, X2_homo
 
-    def ransac(self, img1, kp1, img2, kp2, matches, num_matches=1000, max_iter=10, radius_in_px=10, show_transformed=False):
+    def ransac(self, img1, kp1, img2, kp2, matches, num_matches=100, max_iter=10, radius_in_px=10, show_transformed=False):
         """Performs RANSAC to find best matches."""
 
         best_inlier_count = 0
@@ -162,29 +131,34 @@ class ImageAlignment:
             # choose matches randomly
             selected_matches = np.random.choice(matches, num_matches)
 
-            # get transformation parameters
-            # m1, m2, m3, m4, t1, t2 = self.get_transformation(kp1, kp2, selected_matches)
-            RT, X1_homo, X2_homo = self.get_transformation(kp1, kp2, selected_matches)
+            # get matched keypoints in img1
+            X1 = np.array([kp1[matches[i].queryIdx].pt for i in range(len(matches))])
 
-            # transform points
-            X2_transformed_homo = np.dot(X1_homo, RT.T)
+            # get matched keypoints in img2
+            X2 = np.array([kp2[matches[i].trainIdx].pt for i in range(len(matches))])
+
+            # get transformation parameters
+            params = rigid_body_transform_params(X1, X2)
+            
+            # transform X1 to get X2_transformed
+            X2_transformed = rigid_body_transform(X1, params)
 
             # find inliers
-            diff = np.linalg.norm(X2_transformed_homo - X2_homo, axis=1)
+            diff = np.linalg.norm(X2_transformed - X2, axis=1)
             indices = diff < radius_in_px
             num_inliers = sum(indices)
             if num_inliers > best_inlier_count:
                 print(f"Found {num_inliers} inliers!")
-                show_transformed = True
-                best_params = RT
+                best_params = params
+                best_inlier_count = num_inliers
 
-            if show_transformed:
+                # show matches
                 selected_kp1 = [kp1[m.queryIdx] for m in selected_matches]
                 selected_kp2 = [kp2[m.trainIdx] for m in selected_matches]
                 selected_kp2_transformed = []
                 _selected_matches = []
                 for i in range(len(selected_matches)):
-                    xy = cv2.KeyPoint(*X2_transformed_homo[i, :2].astype("uint8"), size=selected_kp2[i].size)
+                    xy = cv2.KeyPoint(*X2_transformed[i].astype("uint8"), size=selected_kp2[i].size)
                     selected_kp2_transformed.append(xy)
 
                     _match = selected_matches[i]
@@ -196,28 +170,12 @@ class ImageAlignment:
                 show_single_image(img)
                 img = cv2.drawMatches(img1, selected_kp1, img2, selected_kp2_transformed, _selected_matches, outImg=img, matchColor=110, matchesThickness=3, singlePointColor=(0, 0, 0))
                 show_single_image(img)
-
-        import ipdb; ipdb.set_trace()
-        
-
     
     def align(self, img1, kp1, img2, kp2, matches):
         self.ransac(img1, kp1, img2, kp2, matches)
 
 
-if __name__ == "__main__":
-    # read & show images
-    # boat1 = cv2.imread('boat1.pgm', cv2.IMREAD_GRAYSCALE)
-    # boat2 = cv2.imread('boat2.pgm', cv2.IMREAD_GRAYSCALE)
-    # show_two_images(boat1, boat2, title="Sample pair of images.")
-
-    # # get matches
-    # kp_matcher = KeypointMatcher(contrastThreshold=0.1, edgeThreshold=5)
-    # matches, kp1, des1, kp2, des2 = kp_matcher.match(boat1, boat2, show_matches=True)
-
-    # image_alignment = ImageAlignment()
-    # image_alignment.align(boat1, kp1, boat2, kp2, matches)
-
+def test_individual_functions():
     # checking rigid body transformation function
     X = np.array(
         [
@@ -247,5 +205,21 @@ if __name__ == "__main__":
     assert np.linalg.norm(params - est_params) < 1e-8
 
 
-    
+if __name__ == "__main__":
+
+    # testing individual functions
+    test_individual_functions()
+
+    # read & show images
+    boat1 = cv2.imread('boat1.pgm', cv2.IMREAD_GRAYSCALE)
+    boat2 = cv2.imread('boat2.pgm', cv2.IMREAD_GRAYSCALE)
+    show_two_images(boat1, boat2, title="Sample pair of images.")
+
+    # get matches
+    kp_matcher = KeypointMatcher(contrastThreshold=0.1, edgeThreshold=5)
+    matches, kp1, des1, kp2, des2 = kp_matcher.match(boat1, boat2, show_matches=True)
+
+    image_alignment = ImageAlignment()
+    image_alignment.align(boat1, kp1, boat2, kp2, matches)
+
 
