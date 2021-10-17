4b7a9f1ceab6875e198b80f65da91ec8ea7fcde9
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-10-17 17:07:54 +0200

    Minor but important bugfix

--- labfinal1/constants.py
+++ labfinal1/constants.py
@@ -11,6 +11,7 @@ idx_to_class = {k + 1: v for (k, v) in enumerate(classes)}
 
 # classes to be used for classification (copied from stl)
 relevant_classes = np.array([1, 2, 9, 7, 3])
+relevant_classes = np.sort(relevant_classes)
 
 NUM_TRAIN_SAMPLES_PER_CLASS = 500
 NUM_TEST_SAMPLES_PER_CLASS = 800
