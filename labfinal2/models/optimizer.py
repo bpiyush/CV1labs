391fb38e55ab71959f1cd23dca91a2893fc41df0
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-10-21 15:27:16 +0200

    Adds optimizer and scheduler functions

---
+++ labfinal2/models/optimizer.py
@@ -0,0 +1,17 @@
+"""Defines optimizers and schedulers for gradient descent."""
+from torch.optim import SGD, Adam, AdamW
+from torch.optim.lr_scheduler import StepLR, MultiStepLR, ExponentialLR
+
+
+def optimizer(name:str, model_params: dict, **kwargs):
+    """Defines an optimizer."""
+    assert name in ["SGD", "Adam", "AdamW"]
+    opt = eval(name)(params=model_params, **kwargs)
+    return opt
+
+
+def scheduler(opt, name: str, **kwargs):
+    """Defines LR scheduler."""
+    assert name in ["StepLR", "MultiStepLR", "ExponentialLR"]
+    sch = eval(name)(opt, **kwargs)
+    return sch
