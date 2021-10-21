1986208b20baef0fe799e47587e59cf193068511
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-10-21 14:52:04 +0200

    Tests more augmentations transforms

--- labfinal2/data/input_transforms.py
+++ labfinal2/data/input_transforms.py
@@ -23,6 +23,7 @@ class InputTransform:
 
 
 if __name__ == "__main__":
+    # test basic transforms
     transform_list = [
         {
             "name": "ToTensor",
@@ -38,3 +39,46 @@ if __name__ == "__main__":
     x = np.random.randn(32, 32, 3)
     tx = IT(x)
     assert tx.shape == torch.Size([3, 32, 32])
+
+    # test more transforms (augmentations)
+    transform_list = [
+        {
+            "name": "ToTensor",
+            "args": {},
+        },
+        {
+            "name": "Normalize",
+            "args": {"mean": (0.5, 0.5, 0.5), "std": (0.5, 0.5, 0.5)},
+        },
+        {
+            "name": "ColorJitter",
+            "args": dict(brightness=0.2, contrast=0.1, saturation=0.1, hue=0.1),
+        },
+        {
+            "name": "RandomAffine",
+            "args": {"degrees": 30, "translate": (0.1, 0.1), "scale": (0.5, 1.5)},
+        },
+        {
+            "name": "RandomHorizontalFlip",
+            "args": {"p": 0.5},
+        },
+        {
+            "name": "RandomResizedCrop",
+            "args": {"size": 32},
+        },
+        {
+            "name": "GaussianBlur",
+            "args": {"kernel_size": 5},
+        },
+        {
+            "name": "RandomErasing",
+            "args": {},
+        },
+    ]
+    IT = InputTransform(transform_list)
+
+    x = np.random.randn(32, 32, 3)
+    tx = IT(x)
+    assert tx.shape == torch.Size([3, 32, 32])
+
+
