47154b02ace39e6487672c87b1b0de5b4b669649
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-10-23 16:30:47 +0200

    Adds aditional layers to CNN and TLN

--- labfinal2/networks/convnet.py
+++ labfinal2/networks/convnet.py
@@ -37,11 +37,56 @@ class ConvNet(nn.Module):
         x = self.linear3(x)
 
         return x
+
+
+class ConvNetAdditionalLayers(nn.Module):
+    """Adds additional layers to the CNN"""
+    def __init__(self, in_channels, num_classes, act="ReLU"):
+        super(ConvNetAdditionalLayers, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels = 6, kernel_size = 5, stride = 1)
+        self.conv2 = nn.Conv2d(in_channels = 6, out_channels = 16, kernel_size = 5, stride = 1)
+        self.conv3 = nn.Conv2d(in_channels = 16, out_channels = 64, kernel_size = 5, stride = 1)
+        self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 5, stride = 1)
+        self.pool = nn.AvgPool2d(kernel_size = 2, stride = 2)
+
+        self.linear1 = nn.Linear(in_features = 128, out_features = 84)
+        self.linear2 = nn.Linear(in_features = 84, out_features = num_classes)
+
+        self.act_fn = getattr(nn, act)()
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.act_fn(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.act_fn(x)
+
+        x = self.conv3(x)
+        x = self.act_fn(x)
+
+        x = self.conv4(x)
+        x = self.act_fn(x)
+
+        x = self.pool(x)
+        x = x.view(x.size()[0], -1)
+
+        x = self.linear1(x)
+        x = self.act_fn(x)
+    
+        x = self.linear2(x)
+
+        return x
     
 
 if __name__ == "__main__":
     num_classes = 10
     net = ConvNet(in_channels=3, num_classes=num_classes)
     x = torch.randn((1, 3, 32, 32))
+    y = net(x)
+    assert y.shape == torch.Size([1, num_classes])
+
+
+    net = ConvNetAdditionalLayers(3, num_classes)
     y = net(x)
     assert y.shape == torch.Size([1, num_classes])
--- labfinal2/networks/twolayernet.py
+++ labfinal2/networks/twolayernet.py
@@ -30,11 +30,51 @@ class TwolayerNet(nn.Module):
         x = self.act_fn(x)
         scores = self.linear2(x)
         return scores
-    
+
+
+class TwolayerNetAdditionalLayers(nn.Module):
+    # assign layer objects to class attributes
+    # nn.init package contains convenient initialization methods
+    # http://pytorch.org/docs/master/nn.html#torch-nn-init
+    def __init__(self, num_inputs, num_hidden: list, num_classes):
+        '''
+        :param input_size: 3*32*32
+        :param hidden_size: decide by yourself e.g. 1024, 512, 128 ...
+        :param num_classes: 
+        '''
+        self.num_inputs = num_inputs
+        self.num_hidden = num_hidden
+        self.num_classes = num_classes
+        
+        super(TwolayerNetAdditionalLayers, self).__init__()
+        self.act_fn = nn.ReLU()
+
+        self.linear_layers = [nn.Linear(num_inputs, num_hidden[0])]
+        for i in range(1, len(num_hidden)):
+            self.linear_layers.append(nn.Linear(num_hidden[i - 1], num_hidden[i]))
+            self.linear_layers.append(self.act_fn)
+        self.linear_layers = nn.Sequential(*self.linear_layers)
+
+        self.final = nn.Linear(num_hidden[-1], num_classes)
+
+    def forward(self,x):
+        # flatten
+        x = x.view(x.shape[0], -1)
+
+        x = self.linear_layers(x)
+        scores = self.final(x)
+
+        return scores
+
 
 if __name__ == "__main__":
     num_classes = 10
     net = TwolayerNet(num_inputs = 3*32*32, num_hidden = 50, num_classes = num_classes)
     x = torch.randn((1, 3, 32, 32))
+    y = net(x)
+    assert y.shape == torch.Size([1, num_classes])
+
+
+    net = TwolayerNetAdditionalLayers(num_inputs = 3*32*32, num_hidden = [1024, 512, 128], num_classes = num_classes)
     y = net(x)
     assert y.shape == torch.Size([1, num_classes])
