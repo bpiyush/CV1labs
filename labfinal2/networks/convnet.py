294071fc44c286851cdd3ca1af2920879f9ec979
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-10-24 13:02:14 +0200

    Finishes V1

--- labfinal2/networks/convnet.py
+++ labfinal2/networks/convnet.py
@@ -33,14 +33,14 @@ class ConvNet(nn.Module):
 
         x = x.view(x.size()[0], -1)
 
-        if self.return_features:
-            return x
-
         x = self.linear1(x)
         x = self.act_fn(x)
     
         x = self.linear2(x)
         x = self.act_fn(x)
+
+        if self.return_features:
+            return x
 
         x = self.linear3(x)
 
